ff79d719feb8afd21c48af65a326c1c5d2804ba9
chef@txp <info@travancorexp.com>
2022-11-21 07:56:24 -0500

    v0.2  Primitive data types

---
+++ 02-primitive-data.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+// integer data types
+  // short var1;
+  // int var2;
+  // long var3;
+  // long long var4;
+  //
+  // unsigned short var5;
+  // unsigned int var6;
+  // unsigned long var7;
+  // unsigned long long var8 {1234567890123456789};
+  // cout << var8 << endl;
+
+// numeric data type
+  // float fvar1;
+  // double dvar2;
+  // long double lvar3;
+
+// boolean data type
+  // bool bvar1; // (false or true) or (0 or postive value)
+
+  return(0);
+}
